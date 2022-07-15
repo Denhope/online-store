6023930d194a12eab58b5641041b44c2fe4f4013
den_hope <den_hope@mail.ru>
2022-07-16 01:55:37 +0300

    feat: add loginForm

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -1,8 +1,52 @@
 import React, { FC } from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Card, Container, Form, Row } from 'react-bootstrap';
+import { useHistory, useLocation, NavLink } from 'react-router-dom';
+import { RouteNames } from '../router';
+import './Auth.scss';
 
 const AuthPage: FC = () => {
-  return <Container>LOGINPAGE</Container>;
+  const location = useLocation();
+  const isLogin = location.pathname === RouteNames.LOGIN_ROUTE;
+  const history = useHistory();
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: window.innerHeight - 54 }}
+    >
+      <Card style={{ width: 600 }} className="p-5">
+        <h2 className="m-auto ">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
+        <Form className="d-flex flex-column gap-3">
+          <Form.Control
+            className="mt-3"
+            placeholder="Введите ваш email..."
+            // value={email}
+            onChange={(e) => null}
+          />
+          <Form.Control
+            className="mt-3"
+            placeholder="Введите ваш пароль..."
+            // value={password}
+            onChange={(e) => null}
+            type="password"
+          />
+          <div className="form__control">
+            {isLogin ? (
+              <div>
+                Нет аккаунта?
+                <NavLink to={RouteNames.REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
+              </div>
+            ) : (
+              <div>
+                Есть аккаунт? <NavLink to={RouteNames.LOGIN_ROUTE}>Войдите!</NavLink>
+              </div>
+            )}
+
+            <Button onClick={() => null}>{isLogin ? 'Войти' : 'Регистрация'}</Button>
+          </div>
+        </Form>
+      </Card>
+    </Container>
+  );
 };
 
 export default AuthPage;
